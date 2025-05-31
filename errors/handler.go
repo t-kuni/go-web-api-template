22963290577892eb9d3d5635fc20e97cb3db163e
t-kuni <t.kuni.dev@gmail.com>
2025-06-01 00:02:24 +0900

    update

--- errors/handler.go
+++ errors/handler.go
@@ -1,19 +1,33 @@
 package errors
 
 import (
+	"github.com/go-openapi/errors"
 	"github.com/t-kuni/go-web-api-template/domain/infrastructure/system"
 	"net/http"
 )
 
 // NewCustomServeError はカスタムエラーハンドラを生成する関数です
 func NewCustomServeError(logger system.ILogger) func(w http.ResponseWriter, r *http.Request, err error) {
-	return func(w http.ResponseWriter, r *http.Request, err error) {
-		// エラーをログに出力
-		if logger != nil {
-			logger.PanicV2(r, err.Error(), nil)
+	return func(rw http.ResponseWriter, r *http.Request, err error) {
+		switch err.(type) {
+		case *errors.CompositeError,
+			*errors.MethodNotAllowedError,
+			errors.Error:
+			// go-openapi管轄のエラーは標準エラーハンドラであるerrors.ServeErrorに移譲する
+			if logger != nil {
+				logger.WarnWithError(r, err, nil)
+			}
+			errors.ServeError(rw, r, err)
+		case nil:
+			if logger != nil {
+				logger.Panic(r, "Unknown Error", nil)
+			}
+			rw.WriteHeader(http.StatusInternalServerError)
+		default:
+			if logger != nil {
+				logger.PanicV2(r, err.Error(), nil)
+			}
+			rw.WriteHeader(http.StatusInternalServerError)
 		}
-
-		// 常に500エラーを返す
-		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
